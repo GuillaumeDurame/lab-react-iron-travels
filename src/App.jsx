aae8c9201e6b47f96cd9551f90be92330c4d3750
Guillaume Duramé <durameg@gmail.com>
2024-05-05 14:30:51 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import logo from "./assets/logo.png";
 import "./App.css";
+import TravelList from "./components/TravelList";
 
 function App() {
   return (
     <>
       <div>
-        <img src={logo} className="logo" alt="App logo" />
+        <img
+          src={logo}
+          className="logo"
+          alt="App logo"
+        />
       </div>
       <h1 className="text-iron">Iron Travels</h1>
       <h3 className="text-iron">Tailored Travel Plans for Ironhackers</h3>
 
       {/* RENDER YOUR LIST COMPONENT HERE */}
-      
+      <TravelList />
     </>
   );
 }
